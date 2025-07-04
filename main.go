66c1c4d18c8c0b73bfe1d158f6b4badf11fe86a4
shaoliang987 <wushaoliang19870828@gmai.com>
2025-07-04 21:36:07 +0800

    结构体1

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/**
+ * 结构体练习
+ */
+
+type person struct {
+	Name  string
+	Age   int
+	Call  func()
+	Map   map[string]string
+	Ch    chan string
+	Arr   [5]uint8
+	Slice []interface{}
+	Ptr   *int
+}
+
+func main() {
+	person1 := person{
+		Name: "张三1",
+		Age:  18,
+		Call: func() {
+			fmt.Println("call")
+		},
+		Map: map[string]string{
+			"1": "1",
+			"2": "2",
+		},
+		Ch: make(chan string),
+		Arr: [5]uint8{
+			1, 2, 3, 4, 5,
+		},
+		Slice: []interface{}{
+			1, "1", true,
+		},
+		Ptr: new(int),
+	}
+	fmt.Println(person1)
+}
